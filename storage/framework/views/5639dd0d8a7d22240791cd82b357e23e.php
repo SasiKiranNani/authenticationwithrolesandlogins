ba14fda3e01f84a0cad4f4bdb6134a6ea899b08b
SasiKiranNani <psk123143@gmail.com>
2024-09-24 10:40:56 +0530

    Some changes made

---
+++ storage/framework/views/5639dd0d8a7d22240791cd82b357e23e.php
@@ -0,0 +1,2 @@
+<input type="checkbox" <?php echo $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']); ?>>
+<?php /**PATH C:\Users\Sunseaz\Desktop\PHP LARAVEL\laravel projects\authenticationwithrolesandlogins\resources\views/components/checkbox.blade.php ENDPATH**/ ?>
